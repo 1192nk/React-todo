21a7a77a4777abf70f8dc7750924a4aece7c32fd
1192nk <57670066+1192nk@users.noreply.github.com>
2022-02-27 08:47:49 +0900

    InputTdos.jsx内にstyleを定義

--- src/components/InputTodo.jsx
+++ src/components/InputTodo.jsx
@@ -1,9 +1,17 @@
 import React from "react";
 
+const style = {
+  backgroundColor: "rgb(247, 236, 221)",
+  width: "400px",
+  height: "30px",
+  padding: "8px",
+  margin: "8px",
+  borderRadius: "8px"
+};
 export const InputTodo = (props) => {
   const { todoText, onChange, onClick } = props;
   return (
-    <div className="input-area">
+    <div style={style}>
       <input
         placeholder="TODOを入力"
         type="text"
